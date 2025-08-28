cdfd291253ccae51bdce34f1426781dbc62b837e
neojarvis <techadmin@iamneo.ai>
2025-08-28 04:22:16 +0000

    Auto-save-commit-8/28/2025--9:52:15-AM

--- db.sql
+++ db.sql
@@ -4,7 +4,13 @@ use testing;
 CREATE Table dummy(
     id int ,
     name VARCHAR(10),
-    phoneNo int,
-    email VARCHAR(15)
+    create_at DATETIME,
+    is_active ENUM("yes" , "no")
 );
 
+INSERT INTO dummy VALUES
+    (1, 'Kinich', '2025-08-28 10:00:00', 'yes'),
+    (2, 'mualani', '2025-08-28 11:00:00', 'yes'),
+    (3, 'hu tao', '2025-08-28 12:00:00', 'no');
+
+SELECT * FROM dummy
